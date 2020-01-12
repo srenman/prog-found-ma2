e83d15472044221c9b60dd31d10d3eef570d5e46
srenman <48174060+srenman@users.noreply.github.com>
2020-01-12 01:03:48 +0100

    Upload js file

---
+++ script.js
@@ -0,0 +1,53 @@
+// Question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+// Question 2
+
+console.log(petArray[1]);
+
+// Question 3
+
+petArray.push("sheep");
+
+// Question 4
+
+console.log(petArray.length);
+
+// Question 5
+
+var catObject = {
+  name: "Findus",
+  colour: "Black",
+  age: 4
+};
+
+// Question 6
+
+console.log(catObject.age);
+
+// Question 7
+
+var catArray = [
+  {
+    name: "Findus",
+    colour: "Black",
+    age: 4
+  }
+];
+
+// Question 8
+
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i]);
+}
+
+// Question 9
+
+function logToConsole(whatAnimal) {
+  console.log(whatAnimal);
+}
+
+// Question 10
+
+logToConsole("donkey");
